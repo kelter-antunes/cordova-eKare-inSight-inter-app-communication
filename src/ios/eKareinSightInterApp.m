744e32e11ee7a50fb96369fc0e4dff3af1c64a0a
Miguel Antunes <miguel.aka.kelter@gmail.com>
2023-05-29 12:44:12 +0100

    Update eKareinSightInterApp.m

--- src/ios/eKareinSightInterApp.m
+++ src/ios/eKareinSightInterApp.m
@@ -1,4 +1,5 @@
 #import "eKareinSightInterApp.h"
+#import "RNEncryptor.h"
 
 @implementation eKareinSightInterApp
 
@@ -60,11 +61,41 @@ NSString *const PREFIX_ERROR = @"ERR: ";
 
   if ([action isEqualToString:@"open"])
   {
-    NSData *textData = [kInterAppPW dataUsingEncoding:NSUTF8StringEncoding];
-
     
+     // Dictionary to be sent to inSight app which should include the hash that will be returned back
+    NSDictionary *clearDict = @{@"hash": @"43934s341049fjls348434", @"data": @"data value"};
 
-    result = [textData base64EncodedStringWithOptions:0];
+    // Convert the NSDictionary object to NSData object
+    //NSData *measurementsClearData = [NSData dataWithContentsOfFile:[NSBundle.mainBundle pathForResource:@"interapp" ofType:@"dat"]];
+
+    NSData *measurementsClearData = [NSKeyedArchiver archivedDataWithRootObject:clearDict];
+
+    // Encryption password
+    NSString *password = kInterAppPW;
+
+    // Encrypt Data (AES256) using RNEncryptor library
+    NSData *measurementsEncryptedData = [RNEncryptor encryptData:measurementsClearData withSettings:kRNCryptorAES256Settings password:password error:nil];
+    
+    // Convert encrypted data to NSString
+    NSString *measurementsEncryptedString = [measurementsEncryptedData base64EncodedStringWithOptions:0];
+
+    // Prepare the parameters to be passed in the URL
+    NSString *params = [NSString stringWithFormat:@"data=%@", measurementsEncryptedString];
+
+
+    // Prepare the URL string:
+    NSString *scheme = kInterAppScheme;
+    NSString *appId = kInterAppId;
+    NSString *urlString = [NSString stringWithFormat:@"%@://%@?%@", scheme, appId, params];
+
+
+    // Prepare the NSURL that will open inSight app
+    NSURL *url = [NSURL URLWithString:urlString];
+
+    result = [url base64EncodedStringWithOptions:0];
+
+
+
     }
 	else
 	{
